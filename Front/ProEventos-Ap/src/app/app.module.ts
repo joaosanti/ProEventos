0282b2c85270e7f67abbe7768ac2aaf45dd75f94
João Carlos de Santi Junior <jr16rp@yahoo.com.br>
2023-07-13 22:52:46 -0300

    Project Front - Angular

---
+++ Front/ProEventos-Ap/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http'; // Import this line
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
