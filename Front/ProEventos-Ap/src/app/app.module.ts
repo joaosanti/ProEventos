f039a82ca888232225b6b43bb21455888825dc25
João Carlos de Santi Junior <jr16rp@yahoo.com.br>
2023-07-31 19:46:39 -0300

    Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-Ap/src/app/app.module.ts
+++ Front/ProEventos-Ap/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { HttpClientModule } from '@angular/common/http'; // Import this line
 
@@ -7,6 +9,11 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavComponent } from './nav/nav.component';
+
+
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 
@@ -14,12 +21,16 @@ import { BrowserModule } from '@angular/platform-browser';
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
-  ],
+    PalestrantesComponent,
+      NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
